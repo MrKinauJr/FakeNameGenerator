304e467525269a397cb38763f998e25782fe544a
MrKinauJr <46278197+MrKinauJr@users.noreply.github.com>
2019-12-09 15:56:12 +0100

    Made Package installable via PIP

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='FakeNameGenerator',
+    version='0.0.1',
+    description='a pip-installable Fake Data Generator',
+    license='MIT',
+    packages=['FakeNameGenerator'],
+    author='MrKinauJr',
+    author_email='FakeNameGen@mrkinaujr.xyz',
+    keywords=['Fake Data Generator'],
+    url='https://github.com/mrkinaujr/FakeNameGenerator'
+)
